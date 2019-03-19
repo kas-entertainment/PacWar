bbc93d8a9afa190dccd9ebdde5d681574d1d8f22
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-03-18 22:41:11 -0500

    js basic

--- src/main/resources/static/js/Users.js
+++ src/main/resources/static/js/Users.js
@@ -2,16 +2,19 @@ var host = "http://localhost:8080/register/"
 
 
 
-async function getUser(){  
+async function getUser() {
     var agregar = "";
     await Promise.resolve(axios.get(host)
-	.then(async function(response){
-                
-            }));           
+            .then(async function (response) {
+
+            }));
 }
 
-async function addUser(){
-    var room = prompt("coloque el valor requerido")
-    await Promise.resolve(axios.post(host+"?room="+room));
-    await getUser();
+async function addUser() {
+    console.log(document.getElementById("Name"))
+    document.getElementById("Name")
+    document.getElementsByClassName("lastname")
+    document.getElementsByClassName("email")
+    document.getElementsByClassName("nikName")
+    document.getElementsByClassName("password")
 }
